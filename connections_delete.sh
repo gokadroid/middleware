a47b1d26d6183ea7802b6293af5109d9551c0c36
gokadroid <gokadroid@gmail.com>
2020-09-29 10:51:23 -0500

    Update connections_delete.sh

--- connections_delete.sh
+++ connections_delete.sh
@@ -42,7 +42,8 @@ CONFIGFILE=${CONFIGDIR}/ems_config.txt
 # ENV|EMSURL|USERNAME|HOSTPREFIX
 ####################################################################
 
-
+#Error string which script will return when ems is not active
+ERRORSTATE="ERROR-ACTIVE-STATE|ERROR-ACTIVE-STATE"
 
 #
 EMSURLS=
@@ -179,12 +180,12 @@ function is_active()
                 
         else
         #EMS state is neither error or active
-                echo "ERROR-ACTIVE-STATE|ERROR-ACTIVE-STATE"
+                echo ${ERRORSTATE}
         fi
 
 }
 
-#find_aota_connections $TIBEMSADMIN $ACTIVEURL $USER $PWD $SCRIPT
+#run_ems_script $TIBEMSADMIN $ACTIVEURL $USER $PWD $SCRIPT
 function run_ems_script()
 {
         TIBEMSADMIN=${1}
@@ -193,17 +194,17 @@ function run_ems_script()
         PWD=${4}
         SCRIPT=${5}
         
-        ${TIBEMSADMIN} -server ${EMS} -user ${USER} -password ${PWD} -script ${SCRIPT} -ignore
+        ${TIBEMSADMIN} -server ${EMS} -user ${USER} -password ${PWD} -script ${SCRIPT} -ignore | sed '1,10d'
 
 }
 
 
-
+#connectionStatus
 function connectionStatus(){
         #echo "Count Username Destination"
-
-        echo "show connections full" | $1 -server $2 -user $3 -password $4 | sed '1,10d' | grep -v "SASB Pre Dlv" | awk '{print $7" "$8" "$9}' | grep -v ${2} | sort -n | uniq -c
-        echo "Connection Snapshot Complete!"
+	logger " | Taking connection snapshot for "$1
+        echo "show connections full" | $1 -server $2 -user $3 -password $4 | sed '1,10d' | grep -v "SASB Pre Dlv" | awk '{print $4"|"$7"|"$8"|"$9}' 
+        logger " | Connection Snapshot Complete!"
 }
 
 
@@ -215,17 +216,51 @@ function main()
                 ####################################################################
                 # ENV|EMSURL|USERNAME|HOSTPREFIX
                 # ENV|EMSURL|USERNAME|HOSTPREFIX
+		# TXPROD|ssl://f-tibems001lp:14100,ssl://f-tibems002lp:14100|apijavaota|10.20.30
                 ####################################################################
                 URL=`echo ${line} | awk -F"|" '{print $2}'`
+
+                #active_ems $TIBEMSADMIN $URL $USER $PWD $SCRIPT returns URL|EMSNAME
+                URL_AND_NAME=`active_ems ${TIBEMSADMIN} ${URL} ${USER} ${PWD} ${EMSSERVERINFO}`
+                ACTIVE_EMS=`echo ${URL_AND_NAME} | awk -F"|" '{print $1}'`
+                ACTIVE_EMS_NAME=`echo ${URL_AND_NAME} | awk -F"|" '{print $2}'`
+
+		#check if ACTIVE EMS errored out
+		if [ ! -z ${ACTIVE_EMS} || ! -z ${ACTIVE_EMS_NAME} ||  ${ACTIVE_EMS} = 'ERROR-ACTIVE-STATE' || ${ACTIVE_EMS_NAME} = 'ERROR-ACTIVE-STATE' ]
+		then
+			logger " | Error while processing line : "${line}
+			logger " | ACTIVE_EMS or ACTIVE_EMS_NAME check failed"
+			continue
+		fi
+                
+                #Use url to find connections for username and hostprefix and use the name to name all temp files and scripts
                 USERNAME=`echo ${line} | awk -F"|" '{print $3}'`
                 HOSTPREFIX=`echo ${line} | awk -F"|" '{print $4}'`
                 
-                #active_ems $TIBEMSADMIN $URL $USER $PWD $SCRIPT returns URL|EMSNAME
-                URL_AND_NAME=`active_ems ${TIBEMSADMIN} ${URL} ${USER} ${PWD} ${EMSSERVERINFO}`
-                
-                #Use url to find connections for username and hostprefix and use the name to name all temp files and scripts
-                
-               # From connections filter the connection ids and build delete script of connection ids
+		#Get All connections and save it in file using below script
+		#run_ems_script $TIBEMSADMIN $ACTIVEURL $USER $PWD $SCRIPT
+		#Keep all temporary connections files 
+		#TEMPDIR=${SCRIPTHOME}/temp
+		#TEMPCONNECTIONSNAP="_connection_snap.txt"
+		#${TEMPDIR}/${ACTIVE_EMS_NAME}${TEMPCONNECTIONSNAP}
+
+
+		CONNECTION_FILE = ${TEMPDIR}/${ACTIVE_EMS_NAME}${TEMPCONNECTIONSNAP}
+		run_ems_script $TIBEMSADMIN $ACTIVEURL $USER $PWD $SCRIPT | grep -v "SASB Pre Dlv" | awk '{print $4"|"$7"|"$8"|"$9}' > ${CONNECTION_FILE}
+		logger " | Created connection file : "${CONNECTION_FILE}
+		                
+                # From connections filter the connection ids and build delete script of connection ids
+		# Keeps all EMS Scripts
+		# EMSSCRIPTSREPO=${SCRIPTHOME}/scripts/
+		# EMSSERVERINFO=${EMSSCRIPTSREPO}/server_info.sh
+		# Template for EMS specific script
+		# EMSDELTECONNECTIONTEMPLATE="_del_user_connections.sh"
+
+		CONNECTION_SCRIPT = ${EMSSCRIPTSREPO}/${ACTIVE_EMS_NAME}${EMSDELTECONNECTIONTEMPLATE}
+		cat ${CONNECTION_FILE} | grep ${USERNAME} | grep ${HOSTPREFIX} | awk -F"|" '{print $1}' > ${CONNECTION_SCRIPT}
+		logger " | Created delete conenction script : "${CONNECTION_SCRIPT}
+		logger " | This will delete "$(wc - l ${CONNECTION_SCRIPT})" connections"
+		sed -i "s/^/delete connection /g" ${CONNECTION_SCRIPT}
                
                # Execute the delete connections script
 
